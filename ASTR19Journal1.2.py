1c31aec21fc33b8691f5afd63fdeb6498f9952c0
ViridRainDrop <82800293+ViridRainDrop@users.noreply.github.com>
2024-10-18 01:16:39 -0700

    Add files via upload

---
+++ ASTR19Journal1.2.py
@@ -0,0 +1,14 @@
+Float1 = float(input('Enter value of Float:'))
+Float2 = float(input('Enter value of Float:'))
+FLsum = Float1 + Float2
+print(FLsum)
+
+Int1 = int(input('Enter value of Int:'))
+Int2 = int(input('Enter value of Int:'))
+INTDIF = Int1 - Int2
+print(INTDIF)
+
+Float_num = float(input('Enter value of Float:'))
+Int_num = int(input('Enter value of Int:'))
+FLITPRODUC = Float_num * Int_num
+print(FLITPRODUC)
