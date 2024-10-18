2b01a381f037b90a5156a646dcf1dca4d3d9cef2
ViridRainDrop <82800293+ViridRainDrop@users.noreply.github.com>
2024-10-18 01:22:11 -0700

    Update ASTR19Journal1.2.py

--- ASTR19Journal1.2.py
+++ ASTR19Journal1.2.py
@@ -1,14 +1,14 @@
 Float1 = float(input('Enter value of Float:'))
 Float2 = float(input('Enter value of Float:'))
 FLsum = Float1 + Float2
-print(FLsum)
+print('The sum of two floating numbers:', FLsum)
 
 Int1 = int(input('Enter value of Int:'))
 Int2 = int(input('Enter value of Int:'))
 INTDIF = Int1 - Int2
-print(INTDIF)
+print('The different between two integers:', INTDIF)
 
 Float_num = float(input('Enter value of Float:'))
 Int_num = int(input('Enter value of Int:'))
 FLITPRODUC = Float_num * Int_num
-print(FLITPRODUC)
+print('The prodct of a floating number and a integers:', FLITPRODUC)
